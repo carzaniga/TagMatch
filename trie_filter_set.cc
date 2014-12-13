2537d50bb5bdd6fe15a03e2d3b8ceb993523d886
Antonio Carzaniga <(firstname.lastname@usi.ch)>
2014-12-13 16:52:24 +0100

    * trie_filter_set.cc: first minimally working prototype of the   trie_based subset counter.

--- trie_filter_set.cc
+++ trie_filter_set.cc
@@ -8,31 +8,75 @@
 #include "filter_set.hh"
 #include "packet.hh"
 
-struct node {
-	node * zero_link;
+class node {
+public:
 	uint8_t pos;
-	
-	node() : zero_link(nullptr) {};
+
+private:
+	bool have_sibling_table;
+	union {
+		node * next_sibling;
+		node ** siblings;
+	};
+
+static const unsigned int MIN_TABLE_SIZE = 16;
+
+public:
+	node() : have_sibling_table(false), next_sibling(nullptr) {};
+
+	const node * sibling_greater_or_equal(uint8_t p) const {
+		if (have_sibling_table) {
+			return siblings[p];
+		} else {
+			const node * n = this;
+			while (n->pos < p) {
+				n = n->next_sibling;
+				if (n == nullptr)
+					break;
+			}
+			return n;
+		}
+	}
+
+	void consolidate();
+
+	static void append_filter(node ** hook, const filter_t & f);
 };
 
-static node * root = nullptr;
-
-static void clear_r(node * f) {
-	if (!f)
-		return;
-	for(node * n = f; n->pos < filter_t::WIDTH; ++n) 
-		clear_r(n->zero_link);
-	delete[](f);
-}
+node * root = nullptr;
 
 void filter_set::clear() {
-	clear_r(root);
 	root = nullptr;
 }
 
-void filter_set::add(const filter_t & f) {
-	node ** hook = &root;
+void node::consolidate() {
+	if (have_sibling_table)
+		return;
 
+	unsigned int siblings_count = 0;
+	for(const node * n = next_sibling; n != nullptr; n = n->next_sibling)
+		++siblings_count;
+
+	if (siblings_count < MIN_TABLE_SIZE)
+		return;
+
+	node ** table = new node*[filter_t::WIDTH + 1];
+
+	node * n = this;
+	for(uint8_t i = 0; i <= filter_t::WIDTH; ++i) {
+		table[i] = n;
+		if (n != nullptr && i == n->pos) {
+			node * next = n->next_sibling;
+			n->have_sibling_table = true;
+			n->siblings = table;
+			if (n->pos < filter_t::WIDTH)
+				(n + 1)->consolidate();
+			n = next;
+		}
+	}
+}
+
+void node::append_filter(node ** hook, const filter_t & f) {
 	unsigned int prefix_len = 0;
 	unsigned int f_pos = f.next_bit(0); 
 
@@ -46,7 +90,7 @@ void filter_set::add(const filter_t & f) {
 			++n;
 		}
 		assert((*hook)->pos < f_pos);
-		hook = &(n->zero_link);
+		hook = &(n->next_sibling);
 	}
 	assert(prefix_len <= f.popcount());
 
@@ -63,7 +107,17 @@ void filter_set::add(const filter_t & f) {
 	}
 }
 
+void filter_set::add(const filter_t & f) {
+	node::append_filter(&root, f);
+}
+
+void filter_set::consolidate() {
+	if (root != nullptr)
+		root->consolidate();
+}
+
 bool filter_set::find(const filter_t & f) {
+#if 0
 	node * n = root;
 	unsigned int f_pos = f.next_bit(0);
 	while(n != nullptr) {
@@ -73,55 +127,53 @@ bool filter_set::find(const filter_t & f) {
 			++n;
 			f_pos = f.next_bit(f_pos + 1);
 		} else if (f_pos < n->pos) {
-			n = n->zero_link;
+			n = n->next_sibling;
 		} else
 			return false;
 	}
+#endif
 	return  false;
 }
 
-struct matching_point {
-	node * n;
-	unsigned int p;
-
-	void assign(node * n_, unsigned int p_) {
-		n = n_;
-		p = p_;
-	}
-};
-
 size_t filter_set::count_subsets_of(const filter_t & f) {
+	if (!root)
+		return 0;
+
 	size_t result = 0;
 
-	matching_point stack[filter_t::WIDTH];
+	const node * stack[filter_t::WIDTH];
 	unsigned int head = 0;
 
+	unsigned int f_pos = f.next_bit(root->pos);
+	const node * n = root->sibling_greater_or_equal(f_pos);
+	if (n == nullptr)
+		return 0;
 
-	unsigned int f_pos = f.next_bit(0);
-	node * n = root;
-
-	while (n != nullptr) {
-		if(f_pos > n->pos) {
-			n = n->zero_link;
-			continue;
-		} else if (f_pos == n->pos) {
+	for (;;) { // assert(f_pos <= n->pos);
+		if (f_pos == n->pos) {
 			if (f_pos == filter_t::WIDTH) {
 				++result;
 			} else {
-				f_pos = f.next_bit(f_pos + 1);
-				if (n->zero_link != nullptr)
-					stack[head++].assign(n->zero_link, f_pos);
+				stack[head++] = n;
 				++n;
+				f_pos = f.next_bit(n->pos);
 				continue;
 			}
-		} 
-		if (head == 0)
-			return result;
-		--head;
-		n = stack[head].n;
-		f_pos = stack[head].p;
+		} else { // assert(f_pos < n->pos);
+			f_pos = f.next_bit(n->pos);
+			n = n->sibling_greater_or_equal(f_pos);
+			if (n != nullptr)
+				continue;
+		}
+		// pop node from stack
+		do {
+			if (head == 0)
+				return result;
+			n = stack[--head];
+			f_pos = f.next_bit(n->pos + 1);
+			n = n->sibling_greater_or_equal(f_pos);
+		} while (n == nullptr);
 	}
-	return result;
 }
 
 size_t filter_set::count_supersets_of(const filter_t & x) {
