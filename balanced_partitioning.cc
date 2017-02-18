83f9ec4d10bbe910edf344561169846fe2c3593a
Antonio Carzaniga <antonio.carzaniga@usi.ch>
2017-02-18 15:31:02 +0100

    added non-zero-mask partitioning

--- balanced_partitioning.cc
+++ balanced_partitioning.cc
@@ -33,14 +33,13 @@ static void print_usage(const char* progname) {
 }
 
 typedef vector<fib_entry *> fib_t;
-static fib_t fib;
 
 static bool binary_format = false;
 
 static std::ostream* prefixes_output = nullptr;
 static std::ostream* filters_output = nullptr;
 
-static void read_filters(std::istream & input, bool binary_format) {
+static void read_filters(fib_t & fib, std::istream & input, bool binary_format) {
 	fib_entry * f = new fib_entry();
 	if (binary_format) {
 		while(f->read_binary(input)) {
@@ -65,12 +64,12 @@ struct partition_candidate {
 	unsigned int freq[filter_t::WIDTH];
 	struct partition_candidate * next;
 
-	partition_candidate(fib_t::iterator b, fib_t::iterator e)
-		: begin(b), end(e), mask(), used_bits(), next(nullptr) {};
+	partition_candidate(fib_t::iterator b, fib_t::iterator e, partition_candidate * n = nullptr)
+		: begin(b), end(e), mask(), used_bits(), next(n) {};
 
 	partition_candidate(fib_t::iterator b, fib_t::iterator e,
-						const filter_t & m, const filter_t & ub, partition_candidate * n)
-		: begin(b), end(e), mask(m), used_bits(ub), next(n) {};
+						const filter_t & m, const filter_t & ub)
+		: begin(b), end(e), mask(m), used_bits(ub) {};
 
 	void compute_frequencies() {
 		std::memset(freq, 0, sizeof(freq));
@@ -100,8 +99,34 @@ struct has_bit {
 	bool operator()(const fib_entry * f) { return ! f->filter[b]; }
 };
 
-partition_candidate * balanced_partitioning(size_t max_p) {
-	partition_candidate * Q = new partition_candidate(fib.begin(), fib.end());
+partition_candidate * nonzero_mask_partitioning(partition_candidate * P0) {
+	for(;;) {
+		P0->compute_frequencies();
+		unsigned int max_freq = P0->freq[0];
+		unsigned int pivot = 0;
+		for(unsigned int b = 1; b < filter_t::WIDTH; ++b) {
+			if (P0->freq[b] > max_freq) {
+				pivot = b;
+				max_freq = P0->freq[b];
+			}
+			if (P0->freq[b] == P0->size())
+				P0->mask.set_bit(b);
+		}
+		if (max_freq == P0->size())
+			return P0;
+
+		vector<fib_entry *>::iterator middle = std::stable_partition(P0->begin, P0->end,
+																	 has_bit(pivot));
+		partition_candidate * P1 = P0;
+		P0 = new partition_candidate(P0->begin, middle, P0);
+		P1->begin = middle;
+		P1->mask.set_bit(pivot);
+	}
+}
+
+partition_candidate * balanced_partitioning(fib_t::iterator begin, fib_t::iterator end,
+											size_t max_p) {
+	partition_candidate * Q = new partition_candidate(begin, end);
 	if (Q->size() <= max_p)
 		return Q;
 
@@ -127,7 +152,7 @@ partition_candidate * balanced_partitioning(size_t max_p) {
 		partition_candidate * P1 = Q;
 		Q = Q->next;
 		P1->used_bits.set_bit(pivot);
-		P0 = new partition_candidate(P1->begin, middle, P1->mask, P1->used_bits, nullptr);
+		P0 = new partition_candidate(P1->begin, middle, P1->mask, P1->used_bits);
 		P1->begin = middle;
 		P1->mask.set_bit(pivot);
 		if (P0->size() > max_p && P1->size() > max_p) {
@@ -141,6 +166,7 @@ partition_candidate * balanced_partitioning(size_t max_p) {
 			P1->next = Q;
 			Q = P1;
 			P0->next = PT;
+			P0 = nonzero_mask_partitioning(P0);
 			PT = P0;
 		} else if (P0->size() > max_p) {
 			P0->compute_frequencies();
@@ -151,6 +177,7 @@ partition_candidate * balanced_partitioning(size_t max_p) {
 		} else {
 			P1->next = PT;
 			P0->next = P1;
+			P0 = nonzero_mask_partitioning(P0);
 			PT = P0;
 		}
 	}
@@ -194,6 +221,8 @@ int main(int argc, const char* argv[]) {
 	std::ofstream prefixes_file;
 	std::ofstream filters_file;
 	std::ifstream input_file;
+
+	static fib_t fib;
 	
 	if (input_fname != nullptr) {
 		std::ifstream input_file(input_fname);
@@ -202,11 +231,11 @@ int main(int argc, const char* argv[]) {
 			return 1;
 		}
 		std::cerr << "Reading filters..." << std::flush;
-		read_filters(input_file, binary_format);
+		read_filters(fib, input_file, binary_format);
 		input_file.close();
 	} else {
 		std::cerr << "Reading filters..." << std::flush;
-		read_filters(std::cin, binary_format);
+		read_filters(fib, std::cin, binary_format);
 	}
 	std::cerr << "\t\t\t" << std::setw(12) << fib.size() << " filters." << endl;
 	
@@ -242,7 +271,7 @@ int main(int argc, const char* argv[]) {
 
 	high_resolution_clock::time_point start = high_resolution_clock::now();
 
-	partition_candidate * PT = balanced_partitioning(max_size);
+	partition_candidate * PT = balanced_partitioning(fib.begin(), fib.end(), max_size);
 
 	high_resolution_clock::time_point stop = high_resolution_clock::now();
 
@@ -286,6 +315,7 @@ int main(int argc, const char* argv[]) {
 
 	for(fib_t::iterator i = fib.begin(); i != fib.end(); ++i)
 		delete(*i);
+	fib.clear();
 	
 	if (prefixes_output == &prefixes_file) prefixes_file.close();
 	if (filters_output == &filters_file) filters_file.close();
