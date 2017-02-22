686fcf6aacf1de650c0056505a1661188ae5edbd
Antonio Carzaniga <antonio.carzaniga@usi.ch>
2017-02-22 14:26:34 +0100

    improved sample_workload to read only first N filters.

--- sample_workload.cc
+++ sample_workload.cc
@@ -15,6 +15,10 @@
 // Read a workload of N filters (without partitions) and outputs a
 // workload of K filters chosen at random.  This program can be used
 // to obtain smaller workloads from a base (larger) workload.
+//
+// More specifically, sample K filters from the first N input filters.
+// So, this program can also be use simply to extract the first m
+// filters by setting both N=m and K=m.
 // 
 class sampler {
 private:
@@ -41,8 +45,13 @@ public:
 				return false;
 			}
 		} else {
+			--k;
 			return true;
 		}
+	}
+
+	bool needs_more() {
+		return (k > 0);
 	}
 };
 
@@ -51,11 +60,11 @@ static int sample_filters(std::istream & input, std::ostream & output, const boo
 	fib_entry f;
 
 	if (binary_format) {
-		while(f.read_binary(input)) 
+		while(s.needs_more() && f.read_binary(input))
 			if (s.sample())
 				f.write_binary(output);
 	} else {
-		while(f.read_ascii(input)) 
+		while(s.needs_more() && f.read_ascii(input))
 			if (s.sample())
 				f.write_ascii(output);
 	}
@@ -64,7 +73,7 @@ static int sample_filters(std::istream & input, std::ostream & output, const boo
 
 static void print_usage(const char * progname) {
 	std::cout << "usage: " << progname 
-			  << " [in=<filename>] [out=<filename>] [-a] N=<total> K=<choices>"
+			  << " [in=<filename>] [out=<filename>] [-a] N=<total-read> K=<choices>"
 			  << std::endl
 			  << "options:" << std::endl
 			  << "\t-a\t: uses ASCII format for I/O (default is binary)" << std::endl;
